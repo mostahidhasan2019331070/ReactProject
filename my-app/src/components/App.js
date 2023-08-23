5167c8478d79bac16090a8e96df14dc6b0f0f88c
mostahidhasan2019331070 <fahimmostahid70@gmail.com>
2023-08-23 21:05:29 +0600

    Alhamdulillah 2nd commit

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,10 +1,33 @@
 
 import './App.css';
+import React from "react";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList"
 
 function App() {
+  const constacts=[
+    {
+      id:"1",
+      name:"Mostahid",
+      email:"mostahid@gmail.com",
+
+
+    },
+    {
+      id:"2",
+      name:"fahim",
+      email:"fahim@gmail.com",
+
+
+    },
+
+  ];
   return (
     <div>
-      Hello world
+      <Header/>
+      <AddContact/>
+      <ContactList constacts={constacts}/>
     </div>
   );
 }
